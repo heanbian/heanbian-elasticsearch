a5baecbac70ced8c7a3fccf83e64bc95477faa08
mahong <mahong@dgg.net>
2019-06-27 14:24:09 +0800

    1

--- src/main/java/com/heanbian/block/elasticsearch/client/page/HPagination.java
+++ src/main/java/com/heanbian/block/elasticsearch/client/page/HPagination.java
@@ -6,10 +6,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
 import java.util.Arrays;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
@@ -72,36 +71,10 @@ public class HPagination implements Serializable {
 		this.pageCache = pageCache;
 	}
 
-	public static String md5Encode(String inStr) {
-		MessageDigest md5 = null;
-		try {
-			md5 = MessageDigest.getInstance("MD5");
-		} catch (Exception e) {
-			e.printStackTrace();
-			return "";
-		}
-		byte[] byteArray = null;
-		try {
-			byteArray = inStr.getBytes("UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		byte[] md5Bytes = md5.digest(byteArray);
-		StringBuffer hexValue = new StringBuffer();
-		for (int i = 0; i < md5Bytes.length; i++) {
-			int val = md5Bytes[i] & 0xff;
-			if (val < 16) {
-				hexValue.append("0");
-			}
-			hexValue.append(Integer.toHexString(val));
-		}
-		return hexValue.toString();
-	}
-
 	void beforeQuery(int inputPageSize, QueryBuilder inputBuilder) {
 		StringBuilder b = new StringBuilder(Arrays.toString(indices)).append(sortFild).append(inputPageSize)
 				.append(inputBuilder.toString());
-		String md5str = md5Encode(b.toString());
+		String md5str = DigestUtils.md5Hex(b.toString());
 		if (uniqueString == null) {
 			uniqueString = md5str;
 		} else {
