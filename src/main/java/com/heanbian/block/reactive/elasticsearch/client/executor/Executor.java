b085307492a0b3cfb608ec5fe0ccb8ddec9873c6
马洪 <Administrator@USER-20180910GE>
2019-09-20 09:10:25 +0800

    优化

--- src/main/java/com/heanbian/block/reactive/elasticsearch/client/executor/Executor.java
+++ src/main/java/com/heanbian/block/reactive/elasticsearch/client/executor/Executor.java
@@ -2,6 +2,7 @@ package com.heanbian.block.reactive.elasticsearch.client.executor;
 
 import com.heanbian.block.reactive.elasticsearch.client.operator.Operator;
 
+@FunctionalInterface
 public interface Executor {
 
 	<R, S> S exec(Operator<R, S> operator, R request);
--- src/main/java/com/heanbian/block/reactive/elasticsearch/client/operator/Operator.java
+++ src/main/java/com/heanbian/block/reactive/elasticsearch/client/operator/Operator.java
@@ -2,6 +2,7 @@ package com.heanbian.block.reactive.elasticsearch.client.operator;
 
 import java.io.IOException;
 
+@FunctionalInterface
 public interface Operator<R, S> {
 
 	S operator(R request) throws IOException;
